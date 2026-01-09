95d40f143b5d1247ee1dd7a0a990631df16db701
Weko <wekohyk@gmail.com>
2026-01-09 14:24:31 +0800

    refactor: 从 App.vue 的 onMounted 方法中移除加载实用程序调用

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <script setup lang="ts">
 import Loading from '@/components/Loading/index.vue';
 import Toast from '@/components/Toast/index.vue';
-import { onMounted } from 'vue';
-
-onMounted(() => {
-  $utils.loading.show();
-});
 </script>
 
 <template>
